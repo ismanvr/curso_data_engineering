16d9a72a261bede81f12c448be849a4f2c2fbd8f
Ismael Navarro <ismanacu@gmail.com>
2023-11-17 18:03:38 +0000

    AVANCE MODIFICANDO TABLAS STG y avanzando en JINJA

--- models/staging/sql_server_dbo/stg_addresses.sql
+++ models/staging/sql_server_dbo/stg_addresses.sql
@@ -1,3 +1,4 @@
+-- Este modelo extrae datos de la tabla 'addresses'
 with 
 
 source as (
@@ -12,7 +13,7 @@ renamed as (
         address_id,
         zipcode,
         country,
-        address,
+        COALESCE(address, 'N/A') AS address,
         state,
         _fivetran_deleted,
         _fivetran_synced as date_load
